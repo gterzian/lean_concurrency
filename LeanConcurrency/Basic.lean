07348d65972d87021faba8c4335ceb03e29bc35a
gterzian <2792687+gterzian@users.noreply.github.com>
2026-03-01 17:45:49 +0700

    initial project files
    Signed-off-by: gterzian <2792687+gterzian@users.noreply.github.com>

---
+++ LeanConcurrency/Basic.lean
@@ -0,0 +1,6 @@
+import LeanConcurrency.ConcurrencySpec
+
+-- this file currently just imports the translated spec; additional
+-- development can go here later.
+
+def hello := "world"
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanConcurrency
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
